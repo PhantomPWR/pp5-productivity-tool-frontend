6ef511e4df3b485b752b5fde02ea20bd80a23220
Jean de Villiers <jeandevilliers@me.com>
2023-06-08 13:44:09 +0000

    Add registration form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Container } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom';
+import './api/axiosDefault';
+import SignUpForm from './pages/auth/SignUpForm'
+
 
 
 function App() {
@@ -12,7 +15,7 @@ function App() {
           <Switch>
             <Route exact path="/" render={() => <h1>Home Page</h1>} />
             <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-            <Route exact path="/register" render={() => <h1>Register</h1>} />
+            <Route exact path="/register" render={() => <SignUpForm />} />
             <Route render={() => <p>Page not found!</p>} />
           </Switch>
         </Container>
