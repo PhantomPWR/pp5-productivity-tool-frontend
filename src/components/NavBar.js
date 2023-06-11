714e2ac63e207225a9b061cc853921fff2396414
Jean de Villiers <jeandevilliers@me.com>
2023-06-11 14:48:51 +0000

    NavBar Burdger Menu toggle with custom hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,10 +8,13 @@ import {
 } from '../contexts/CurrentUserContext';
 import axios from 'axios';
 import Avatar from './Avatar';
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -87,7 +90,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to='/'>
           <Navbar.Brand className={styles.Brand}>
@@ -96,7 +99,11 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addTaskIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto text-start">
             <NavLink
