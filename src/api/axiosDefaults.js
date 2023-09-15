92b5a35a59dd7864ece48b537dae7ba940c6e156
Jean de Villiers <jeandevilliers@me.com>
2023-09-15 08:35:48 +0000

    Maintenance - Code comments & clean-up

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,15 @@
+// Axios library
 import axios from "axios";
 
+// URL for API requests
 axios.defaults.baseURL = 'https://pp5-productivity-tool.herokuapp.com/'
+
+// Set up default headers
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+
+// Set up default withCredentials
 axios.defaults.withCredentials = true
 
+// Set up interceptors to add CSRF token to requests
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
