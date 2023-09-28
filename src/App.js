f036631b064ff0af9399677ab0623b212603ee65
Jean de Villiers <jeandevilliers@me.com>
2023-09-28 23:34:36 +0000

    Fix /register route - Remove path from signin form route - Move register router higher up in the list

--- src/App.js
+++ src/App.js
@@ -37,76 +37,118 @@ import TaskEditForm from './pages/tasks/TaskEditForm';
 import CategoryCreateForm from './pages/categories/CategoryCreateForm';
 import CategoryEditForm from './pages/categories/CategoryEditForm';
 import ProfileEditForm from './pages/profiles/ProfileEditForm';
-import UsernameForm from "./pages/profiles/UsernameForm";
+import UsernameForm from './pages/profiles/UsernameForm';
 import UserPasswordForm from './pages/profiles/UserPasswordForm';
 
-
 function App() {
-  const currentUser = useCurrentUser();
-  const location = useLocation();
+	const currentUser = useCurrentUser();
+	const location = useLocation();
 
-  return (
-    <div className={styles.App}>
-      <NavBar />
-        <Container className={styles.Main}>
-        {!currentUser ? (
-          <Switch location={location}>
-            <Route
-              path="/"
-              render={() => <SignInForm />}
-            />
-            <Route exact path="/signin" render={() => <SignInForm />} />
-            <Route exact path="/register" render={() => <SignUpForm />} />
-          </Switch>
-        ) : (
-          <Switch location={location}>
-            <Route exact path="/" render={() => <HomePage />} />
-            <Route exact path="/home" render={() => <HomePage />} />
-            <Route exact path="/dashboard" render={() => <DashboardPage />} />
-            <Route exact path="/tasks/create" render={() => <TaskCreateForm />} />
-            <Route exact path="/tasks/:id/edit" render={() => <TaskEditForm />} />
-            <Route exact path="/tasks/:id" render={() => <TaskPage />} />
-            <Route exact path="/tasks/" render={() => <TaskList />} />
-            <Route exact path="/profiles/" render={() => <ProfileList />} />
-            <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
-            <Route exact path="/categories/" render={() => <CategoryList />} />
-            <Route exact path="/categories/create" render={() => <CategoryCreateForm />} />
-            <Route exact path="/categories/:id/edit" render={() => <CategoryEditForm />} />
-            <Route exact path="/categories/:id" render={() => <CategoryPage />} />
-            <Route
-              exact
-              path="/profiles/:id/edit/username"
-              render={() => <UsernameForm />}
-            />
-            <Route
-              exact
-              path="/profiles/:id/edit/password"
-              render={() => <UserPasswordForm />}
-            />
-            <Route
-              exact
-              path="/profiles/:id/edit"
-              render={() => <ProfileEditForm />}
-            />
-            <Route render={() => <PageNotFound />} />
-          </Switch>
-          )
-        }
-        </Container>
+	return (
+		<div className={styles.App}>
+			<NavBar />
+			<Container className={styles.Main}>
+				{!currentUser ? (
+					<Switch location={location}>
+						<Route
+							exact
+							path='/register'
+							render={() => <SignUpForm />}
+						/>
+						<Route render={() => <SignInForm />} />
+					</Switch>
+				) : (
+					<Switch location={location}>
+						<Route exact path='/' render={() => <HomePage />} />
+						<Route exact path='/home' render={() => <HomePage />} />
+						<Route
+							exact
+							path='/dashboard'
+							render={() => <DashboardPage />}
+						/>
+						<Route
+							exact
+							path='/tasks/create'
+							render={() => <TaskCreateForm />}
+						/>
+						<Route
+							exact
+							path='/tasks/:id/edit'
+							render={() => <TaskEditForm />}
+						/>
+						<Route
+							exact
+							path='/tasks/:id'
+							render={() => <TaskPage />}
+						/>
+						<Route
+							exact
+							path='/tasks/'
+							render={() => <TaskList />}
+						/>
+						<Route
+							exact
+							path='/profiles/'
+							render={() => <ProfileList />}
+						/>
+						<Route
+							exact
+							path='/profiles/:id'
+							render={() => <ProfilePage />}
+						/>
+						<Route
+							exact
+							path='/categories/'
+							render={() => <CategoryList />}
+						/>
+						<Route
+							exact
+							path='/categories/create'
+							render={() => <CategoryCreateForm />}
+						/>
+						<Route
+							exact
+							path='/categories/:id/edit'
+							render={() => <CategoryEditForm />}
+						/>
+						<Route
+							exact
+							path='/categories/:id'
+							render={() => <CategoryPage />}
+						/>
+						<Route
+							exact
+							path='/profiles/:id/edit/username'
+							render={() => <UsernameForm />}
+						/>
+						<Route
+							exact
+							path='/profiles/:id/edit/password'
+							render={() => <UserPasswordForm />}
+						/>
+						<Route
+							exact
+							path='/profiles/:id/edit'
+							render={() => <ProfileEditForm />}
+						/>
+						<Route render={() => <PageNotFound />} />
+					</Switch>
+				)}
+			</Container>
 
-        {currentUser && (
-          <>
-            {/* Scroll-to-top button */}
-            <ScrollTop />
+			{currentUser && (
+				<>
+					{/* Scroll-to-top button */}
+					<ScrollTop />
 
-            {/* Footer */}
-            <div className={styles.Footer}>
-              <CopyRightContact />
-            </div>
-          </>
-        )}
-    </div>
-  );
+					{/* Footer */}
+					<div className={styles.Footer}>
+						<CopyRightContact />
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default App;
--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,9 +1,8 @@
 // React library & hooks
-import React from "react";
+import React from 'react';
 
 // react-router-dom components for page navigation
-import { Link, useLocation } from "react-router-dom";
-
+import { Link, useLocation } from 'react-router-dom';
 
 // Bootstrap components
 import Container from 'react-bootstrap/Container';
@@ -11,69 +10,78 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 // Styles
-import appStyles from "../../App.module.css";
-import styles from "../../styles/Profile.module.css";
+import appStyles from '../../App.module.css';
+import styles from '../../styles/Profile.module.css';
 
 // Assets
-import Avatar from "../../components/Avatar";
-
+import Avatar from '../../components/Avatar';
 
 const Profile = (props) => {
+	// Set up state variables
+	const location = useLocation();
 
-  // Set up state variables
-  const location = useLocation();
+	// Destructure props
+	const { profile } = props;
+	const { id, image, owner } = profile;
 
-  // Destructure props
-  const { profile } = props;
-  const { id, image, owner } = profile;
+	// Check if on dashboard page
+	const isDashboardPage = location.pathname === '/dashboard';
 
-  // Check if on dashboard page
-  const isDashboardPage = location.pathname === '/';
-
-
-  return (
-    <>
-    {isDashboardPage ? (
-        <Link
-          className={`align-self-center ${styles.ProfileLink}`}
-          to={`/profiles/${id}`}
-          aria-label="Profile"
-        >
-          <Container
-            className={`my-3 py-2 d-flex align-items-center justify-content-center ${isDashboardPage ? styles.DashboardProfile : styles.Profile}`}
-          >
-            <Row className='flex-column align-items-center justify-content-center g-0'>
-              <Col className='text-center'>
-                <Avatar src={image} height={45} />
-              </Col>
-              <Col className={`text-center mt-1 mt-sm-0 fs-6 ${appStyles.TextBold}`}>
-                {owner}
-              </Col>
-            </Row>
-          </Container>
-        </Link>
-      ) : (
-          <Link
-            className={`align-self-start ${styles.ProfileLink}`}
-            to={`/profiles/${id}`}
-            aria-label="Profile"
-          >
-          <Container
-            className={`my-3 py-2 d-flex align-items-center justify-content-start ${isDashboardPage ? styles.DashboardProfile : styles.Profile}`}
-          >
-            <Row className='align-items-center justify-content-start g-0'>
-              <Col>
-                <Avatar src={image} height={45} />
-              </Col>
-              <Col className={`text-center mt-1 mt-sm-0 fs-6 ${appStyles.TextBold}`}>
-                {owner}
-              </Col>
-            </Row>
-          </Container>
-        </Link>
-    )}
-    </>
-  );
+	return (
+		<>
+			{isDashboardPage ? (
+				<Link
+					className={`align-self-center ${styles.ProfileLink}`}
+					to={`/profiles/${id}`}
+					aria-label='Profile'
+				>
+					<Container
+						className={`my-3 py-2 d-flex align-items-center justify-content-center ${
+							isDashboardPage
+								? styles.DashboardProfile
+								: styles.Profile
+						}`}
+					>
+						<Row className='flex-column align-items-center justify-content-center g-0'>
+							<Col className='text-center'>
+								<Avatar src={image} height={45} />
+							</Col>
+							<Col
+								className={`text-center mt-1 mt-sm-0 fs-6 ${appStyles.TextBold}`}
+							>
+								{owner}
+							</Col>
+						</Row>
+					</Container>
+				</Link>
+			) : (
+				<Link
+					className={`align-self-start ${styles.ProfileLink}`}
+					to={`/profiles/${id}`}
+					aria-label='Profile'
+				>
+					<Container
+						className={`my-3 py-2 d-flex align-items-center justify-content-start ${
+							isDashboardPage
+								? styles.DashboardProfile
+								: styles.Profile
+						}`}
+					>
+						<Row className='align-items-center justify-content-start g-0'>
+							<Col>
+								<Avatar src={image} height={45} />
+							</Col>
+							<Col
+								className={`text-center mt-1 mt-sm-0 fs-6 ${appStyles.TextBold}`}
+							>
+								{owner}
+							</Col>
+						</Row>
+					</Container>
+				</Link>
+			)}
+		</>
+	);
 };
 
 export default Profile;
